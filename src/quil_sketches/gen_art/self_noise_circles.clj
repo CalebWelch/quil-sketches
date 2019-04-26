5dd33d3717a489b3dde8a7753689fb7bec28093b
Welch <CW056762@cerner.net>
2019-04-26 16:03:16 -0500

    Added some movement

--- src/quil_sketches/gen_art/self_noise_circles.clj
+++ src/quil_sketches/gen_art/self_noise_circles.clj
@@ -6,15 +6,16 @@
 
 
 (defn setup []
-
-  (q/no-loop)
+  (q/frame-rate 20)
   (q/smooth))
 
 (defn draw-line-color [& line]
   (q/stroke-weight 10)
-  (q/stroke (q/random 0.5 1) (q/random 0.5 1) (q/random 0.5 1))
-  (q/line (nth line 0) (nth line 1)
-          (nth line 2) (nth line 3))
+  (let [rads (q/sin (q/radians (q/frame-count)))
+        color (if (neg? rads) 0 rads)]
+    (q/stroke color (+ color 0.5) (q/random 0.5 1))
+    (q/line (nth line 0) (nth line 1)
+            (nth line 2) (nth line 3)))
   )
 
 (defn draw []
@@ -26,19 +27,27 @@
         line-args (line-join-points xs ys)]
     (let [image1 (q/create-graphics (q/width) (q/height))
           image2 (q/create-graphics (q/width) (q/height))]
-      (q/with-graphics image1
-                       (q/background 0)
-                       (q/color-mode :hsb 1)
-                       (q/background 0)
-                       (dorun (map #(apply draw-line-color %) line-args)))
-      (q/image image1 0 0)
+
+      (println rads)
+      ;Text Image
       (q/with-graphics image2
-                       (q/blend-mode :overlay)
-                       (q/fill 255 255 255)
+                       (q/color-mode :hsb 1)
+                       (q/fill 1 0.1 1)
+                       (q/background 0)
                        (q/text-align :center :center)
-                       (q/text-size 40)
+                       (q/text-size 100)
                        (q/text "L Y S" (/ (q/width) 2) (/ (q/height) 2)))
       (q/image image2 0 0)
+
+      ;Background image
+      (q/with-graphics image1
+                       (q/color-mode :hsb 1)
+                       (q/background 0)
+                       (dorun (map #(apply draw-line-color %) line-args))
+                       (q/blend-mode :overlay)
+                       ;(q/mask-image image2)
+                       )
+      (q/image image1 0 0)
       ))
   )
 
